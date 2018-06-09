e80945ce256031e8ad4369a644f525ab617bc0f6
Autch <autch@autch.net>
2018-06-09 20:29:24 +0900

    MinGW/MSVC compatibility

--- include/sleep.h
+++ include/sleep.h
@@ -5,7 +5,12 @@
 // 現在のタイムスライスを放棄するマクロ OS_YIELD() を定義する。
 // ミリ秒単位でのディレイには SDL_Delay() を使う。
 
+#ifndef _WINDOWS
 #include <sched.h>
 #define OS_YIELD() sched_yield()
+#else
+#include <windows.h>
+#define OS_YIELD() Sleep(0)
+#endif 
 
 #endif // !SLEEP_H
